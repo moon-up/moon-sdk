ed89f43b6d8d92394254075794b2c69c8ca848d5
Eliot Whalan <ewhal@pantsu.cat>
2024-01-10 21:07:46 +1000

    feat: add router and flesh out chatgpt example

--- examples/moon-sdk-authentication-chat-example-app/src/index.tsx
+++ examples/moon-sdk-authentication-chat-example-app/src/index.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import Callback from './pages/callback';
+import ChatGPT from './pages/chatgpt';
+import Ethers from './pages/ethers';
+import Login from './pages/login';
+import OAuth2 from './pages/oauth2';
+import Signup from './pages/signup';
+import Siwe from './pages/siwe';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-	document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-	<React.StrictMode>
-		<Router>
-			<App />
-		</Router>
-	</React.StrictMode>
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/callback" Component={Callback} />
+        <Route path="/chatgpt" Component={ChatGPT} />
+        <Route path="/ethers" Component={Ethers} />
+        <Route path="/login" Component={Login} />
+        <Route path="/oauth2" Component={OAuth2} />
+        <Route path="/signup" Component={Signup} />
+        <Route path="/siwe" Component={Siwe} />
+        <Route path="/" Component={App} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- examples/moon-sdk-authentication-chat-example-app/src/pages/chatgpt.tsx
+++ examples/moon-sdk-authentication-chat-example-app/src/pages/chatgpt.tsx
@@ -4,49 +4,50 @@ import { useEffect, useState } from 'react';
 import { useMoonSDK } from '../hooks/moon';
 
 function ChatInterface() {
-	const [message, setMessage] = useState('');
-	const [chain, setChain] = useState<any>(null);
-	const { moon } = useMoonSDK();
+  const [message, setMessage] = useState('');
+  const [chain, setChain] = useState<any>(null);
+  const { moon } = useMoonSDK();
+  const [results, setResults] = useState<any[]>([]);
 
-	useEffect(() => {
-		const initializeChat = async () => {
-			const chatModel = new ChatOpenAI({
-				modelName: 'gpt-4-0613',
-				temperature: 0,
-				openAIApiKey: process.env.REACT_APP_OPENAI_API_KEY,
-			});
-			const chain = await createOpenAPIChain(
-				'https://vault-api.usemoon.ai/.well-known/swagger.json',
-				{
-					llm: chatModel,
-					headers: {
-						Authorization: `Bearer ${moon?.getMoonAccount().getToken()}`,
-					},
-				}
-			);
-			setChain(chain);
-		};
+  useEffect(() => {
+    const initializeChat = async () => {
+      const chatModel = new ChatOpenAI({
+        modelName: 'gpt-4-1106-preview',
+        temperature: 0,
+        openAIApiKey: process.env.REACT_APP_OPENAI_API_KEY,
+      });
+      const chain = await createOpenAPIChain(
+        'https://vault-api.usemoon.ai/.well-known/swagger.json',
+        {
+          llm: chatModel,
+          headers: {
+            Authorization: `Bearer ${moon?.getMoonAccount().getToken()}`,
+          },
+        }
+      );
+      setChain(chain);
+    };
 
-		initializeChat();
-	}, []);
+    initializeChat();
+  }, []);
 
-	const sendMessage = async () => {
-		if (chain) {
-			const result = await chain.run(message);
-			console.log(JSON.stringify(result, null, 2));
-		}
-	};
+  const sendMessage = async () => {
+    if (chain) {
+      const result = await chain.run(message);
+      console.log(JSON.stringify(result, null, 2));
+      setResults((prevResults) => [...prevResults, result]);
+    }
+  };
 
-	return (
-		<div>
-			<input
-				type="text"
-				value={message}
-				onChange={(e) => setMessage(e.target.value)}
-			/>
-			<button onClick={sendMessage}>Send</button>
-		</div>
-	);
+  return (
+    <div>
+      <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} />
+      <button onClick={sendMessage}>Send</button>
+      {results.map((result, index) => (
+        <p key={index}>{JSON.stringify(result)}</p>
+      ))}
+    </div>
+  );
 }
 
 export default ChatInterface;
