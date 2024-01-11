1352201ace83a9e984455a04989fcb79e40444a1
Eliot Whalan <ewhal@pantsu.cat>
2024-01-11 12:57:51 +1000

    feat: add homepage with all links to demos

--- examples/moon-sdk-authentication-chat-example-app/src/App.tsx
+++ examples/moon-sdk-authentication-chat-example-app/src/App.tsx
@@ -1,15 +1,39 @@
-import { Route, Routes } from 'react-router-dom';
-import SiwePage from './pages/siwe'; // Import the SiwePage component
+import { Link } from 'react-router-dom';
 
 const App = () => {
-	return (
-		<Routes>
-			<Route path="/siwe" element={<SiwePage />} />
-			<Route path="/email-login"></Route>
-			<Route path="/email-signup" element></Route>
-			{/* Other routes... */}
-		</Routes>
-	);
+  return (
+    <div>
+      <div>
+        <Link to="/callback">
+          <button>Callback</button>
+        </Link>
+        <Link to="/chatgpt">
+          <button>ChatGPT</button>
+        </Link>
+        <Link to="/ethers">
+          <button>Ethers</button>
+        </Link>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/oauth2">
+          <button>OAuth2</button>
+        </Link>
+        <Link to="/signup">
+          <button>Signup</button>
+        </Link>
+        <Link to="/siwe">
+          <button>Siwe</button>
+        </Link>
+        <Link to="/whisper">
+          <button>Whisper</button>
+        </Link>
+        <Link to="/">
+          <button>Home</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default App;
