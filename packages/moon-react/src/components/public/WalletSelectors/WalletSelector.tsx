d4e4030a2f3cbbec5b8c6d2696a9239c71722bcc
Munchies <munchies@bytemasons.com>
2024-07-01 15:09:37 +1000

    Add gas balance display to wallet selector - Import useFetchWalletBalances, IconGas, ethers - Update WalletSelector component to show gas balance for each wallet

--- packages/moon-react/src/components/public/WalletSelectors/WalletSelector.tsx
+++ packages/moon-react/src/components/public/WalletSelectors/WalletSelector.tsx
@@ -3,6 +3,9 @@
 import React, { useEffect } from "react";
 import { useMoonSDK } from "../../..";
 import { shortenAddress } from "../../../utils/shortenAddress";
+import { useFetchWalletBalances } from "@/hooks/useWalletGasBalances";
+import { IconGas } from "@/assets/icons";
+import { ethers } from "ethers";
 
 type WalletSelectorProps = {
   selectProps?: React.SelectHTMLAttributes<HTMLSelectElement>;
@@ -10,8 +13,16 @@ type WalletSelectorProps = {
 };
 
 const WalletSelector = ({ selectProps, optionProps }: WalletSelectorProps) => {
-  const { wallets, setWallet, wallet } = useMoonSDK();
-
+  const { wallets, setWallet, wallet, chain } = useMoonSDK();
+  const gasBalanceQueries = useFetchWalletBalances(
+    wallets,
+    chain?.chain_id?.toString() || "1"
+  );
+  // console.log(
+  //   "gasBalanceQueries",
+  //   gasBalanceQueries.map((r) => r.data),
+  //   gasBalanceQueries.map((r) => r.error)
+  // );
   useEffect(() => {
     if (!wallet && wallets.length > 0) setWallet(wallets[0]);
   }, [wallet, wallets, setWallet]);
@@ -23,9 +34,13 @@ const WalletSelector = ({ selectProps, optionProps }: WalletSelectorProps) => {
       onChange={(e) => setWallet(e.target.value)}
       {...selectProps}
     >
-      {wallets.map((wal: string) => (
+      {wallets.map((wal: string, i: number) => (
         <option key={wal} value={wal} className="p-2" {...optionProps}>
-          {shortenAddress(wal)}
+          {shortenAddress(wal)} - <IconGas className="w-6 h-6" />{" "}
+          {ethers?.utils.formatUnits(
+            gasBalanceQueries[i].data || "0",
+            (chain?.native_currency as any)?.decimals || 18
+          )}
         </option>
       ))}
     </select>
