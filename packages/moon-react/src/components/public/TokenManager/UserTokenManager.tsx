8ec612d07e776b0c7394643e4e5427e3ccacaeac
Munchies <munchies@bytemasons.com>
2024-07-04 08:33:17 +1000

    Update token management component: - Added state for token info modal and fetched wallet balances - Implemented fetching and displaying token balances with prices - Improved UI to show gas token and updated tokens with balance & context menu functionality

--- packages/moon-react/src/components/public/TokenManager/UserTokenManager.tsx
+++ packages/moon-react/src/components/public/TokenManager/UserTokenManager.tsx
@@ -5,19 +5,26 @@ import {
   useCoinGeckoPrices,
   useTokenInfo,
   useTokenList,
-} from '@/utils/react-coinghecko';
+} from "@/utils/react-coinghecko";
 import TokenListItem from "./components/TokenListItem";
 import TokenModal from "./components/TokenModal";
 import { UserToken } from "./types";
 import { useMoonSDK } from "@/index";
 import Button from "@/components/Button/Button";
+import { useTokenBalances } from "@/hooks/useTokenBalances";
+import { ethers } from "ethers";
+import TokenInfoModal from "../TokenInfoModal/TokenInfoModal";
+import { useFetchWalletBalances } from "@/hooks/useWalletGasBalances";
 
 type UserTokenManagerProps = {
   dbAdapter: UserTokenDBAdapter;
 };
 
-export const UserTokenManager: React.FC<UserTokenManagerProps> = ({ dbAdapter }) => {
-  const { chains } = useMoonSDK();
+export const UserTokenManager: React.FC<UserTokenManagerProps> = ({
+  dbAdapter,
+}) => {
+  const [tokenInfoModalCGID, setTokenInfoModalCGID] = useState<string>("");
+  const { wallet, chains, chain, session } = useMoonSDK();
   const [cgToken, setCgToken] = useState<string>("");
   const [tokens, setTokens] = useState<UserToken[]>([]);
   const [newToken, setNewToken] = useState<UserToken>({
@@ -30,23 +37,52 @@ export const UserTokenManager: React.FC<UserTokenManagerProps> = ({ dbAdapter })
     icon: "",
   });
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [autocompleteOptions, setAutocompleteOptions] = useState<TokenListItemType[]>([]);
+  const [autocompleteOptions, setAutocompleteOptions] = useState<
+    TokenListItemType[]
+  >([]);
   const [showAutocomplete, setShowAutocomplete] = useState(false);
   const [platforms, setPlatforms] = useState<{ [key: string]: string }>({});
   const [selectedPlatform, setSelectedPlatform] = useState<string>("");
   const [contextMenuOpen, setContextMenuOpen] = useState<string | null>(null);
 
-  const { data: tokenPrices } = useCoinGeckoPrices(tokens.map((token) => token.coinGeckoId));
+  const tokenBalanceQueries = useTokenBalances(wallet, tokens);
+  const gasBalanceQueries = useFetchWalletBalances(
+    [wallet || ""],
+    chain?.chain_id?.toString() || "1"
+  );
+  const { data: tokenPrices } = useCoinGeckoPrices(
+    tokens.map((token) => token.coinGeckoId),
+    {},
+    dbAdapter
+  );
   const { data: tokenInfo } = useTokenInfo(newToken.coinGeckoId || "");
   const { data: tokenList } = useTokenList();
-  console.log("tokenPrices", tokenPrices, tokenInfo, tokenList);
+
+  useEffect(() => {
+    // dbAdapter
+    //   .saveTokenPrice("oath", 0.6)
+    //   .then((res) => console.log("tokenPrice", res));
+    // dbAdapter
+    //   .getTokenPrice("oath")
+    //   .then((res) => console.log("tokenPrice", res));
+  }, []);
+
+  // console.log(
+  //   "tokenPrices",
+  //   tokenPrices,
+  //   tokenInfo,
+  //   tokenList,
+  //   tokenBalanceQueries.map((r) => r.data),
+  //   tokenBalanceQueries.map((r) => r.error)
+  // );
 
   useEffect(() => {
     (async () => {
       const fetchedTokens = await dbAdapter.getTokens();
+      console.log("fetchedTokens", fetchedTokens);
       setTokens(fetchedTokens);
     })();
-  }, [dbAdapter]);
+  }, [dbAdapter, session?.user.id]);
 
   useEffect(() => {
     if (cgToken && tokenList) {
@@ -67,7 +103,6 @@ export const UserTokenManager: React.FC<UserTokenManagerProps> = ({ dbAdapter })
         name: tokenInfo.name,
         symbol: tokenInfo.symbol,
         icon: tokenInfo.image.small,
-        chainId: 1,
         address: Object.values(tokenInfo.platforms)[0],
       }));
       setPlatforms(tokenInfo.platforms);
@@ -115,7 +150,10 @@ export const UserTokenManager: React.FC<UserTokenManagerProps> = ({ dbAdapter })
 
   const handlePlatformChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const chainName = e.target.value;
-    let chain = chains.find((chain) => chain.name === chainName);
+    let chain = chains.find((chain) =>
+      chain.name?.toLocaleLowerCase().includes(chainName.toLocaleLowerCase())
+    );
+    console.log("handlePlatformChange", chain, chainName, platforms[chainName]);
     setNewToken((prevToken) => ({
       ...prevToken,
       chainId: chain?.chain_id || 1,
@@ -128,26 +166,84 @@ export const UserTokenManager: React.FC<UserTokenManagerProps> = ({ dbAdapter })
     setContextMenuOpen((prev) => (prev === address ? null : address));
   };
 
+  //merge balance with token
+  let tokensWithBalance = tokens
+    .map((token, i) => {
+      return {
+        ...token,
+        balance: tokenBalanceQueries[i]?.data
+          ? ethers?.utils.formatUnits(
+              tokenBalanceQueries[i]?.data || "0",
+              token.decimals
+            )
+          : "0",
+        price: tokenPrices?.[token.coinGeckoId] || 0,
+      };
+    })
+    .sort((a, b) => b.chainId - a.chainId);
+
   return (
     <div className="p-4">
-      {tokens.length === 0 && <p>No tokens added yet</p>}
+      {tokens.length === 0 && (
+        <p className="ml-auto mr-auto mb-3 text-text-secondary">
+          No tokens added yet
+        </p>
+      )}
       <ul className="mb-3">
-        {tokens.map((token) => (
+        {chain?.native_currency && (
+          <TokenListItem
+            key={(chain?.native_currency as any).address}
+            token={{
+              isGasToken: true,
+              icon: "https://etherscan.io/images/ethereum-icon.png",
+              name: (chain?.native_currency as any).name,
+              symbol: (chain?.native_currency as any).symbol,
+              coinGeckoId: (chain?.native_currency as any).coinGeckoId,
+              decimals: (chain?.native_currency as any).decimals,
+              address: "0x",
+              chainId: chain?.chain_id || 1,
+              balance: gasBalanceQueries[0]?.data
+                ? ethers?.utils.formatUnits(
+                    gasBalanceQueries[0]?.data || "0",
+                    (chain?.native_currency as any).decimals
+                  )
+                : "0",
+              price:
+                tokenPrices?.[(chain?.native_currency as any).coinGeckoId] || 0,
+            }}
+            onDotsClick={() => {}}
+            contextMenuOpen={false}
+            onRemove={() => {}}
+            onSend={() => alert("Send functionality not implemented")}
+            onEdit={() => alert("Edit functionality not implemented")}
+            onInfo={() => {}}
+          />
+        )}
+        {tokensWithBalance.map((token, i) => (
           <TokenListItem
             key={token.address}
             token={token}
-            onDotsClick={() => toggleContextMenu(token.address)}
-            contextMenuOpen={contextMenuOpen === token.address}
+            onDotsClick={() => toggleContextMenu(token.address + token.chainId)}
+            contextMenuOpen={contextMenuOpen === token.address + token.chainId}
             onRemove={() => handleRemoveToken(token.address)}
             onSend={() => alert("Send functionality not implemented")}
             onEdit={() => alert("Edit functionality not implemented")}
-            onInfo={() => alert("Info functionality not implemented")}
+            onInfo={() => setTokenInfoModalCGID(token.coinGeckoId)}
           />
         ))}
       </ul>
-      <Button onClick={toggleModal} className="btn btn-primary mb-4" color="accentColor">
+      <Button
+        onClick={toggleModal}
+        className="btn btn-primary mb-4"
+        color="accentColor"
+      >
         Add Token
       </Button>
+      <TokenInfoModal
+        isOpen={!!tokenInfoModalCGID}
+        toggleModal={() => setTokenInfoModalCGID("")}
+        cgTokenId={tokenInfoModalCGID}
+      />
       <TokenModal
         isOpen={isModalOpen}
         toggleModal={toggleModal}
