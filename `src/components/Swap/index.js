7f2aee117072d39f16b1e833e8e0c104185ffdca
ewhal <ewhal@bytemasons.com>
2024-12-03 11:02:27 +1000

    feat: remove Swap components and App entry point; update moon-api package.json for module support

--- `src/components/Swap/index.js
+++
@@ -1,6 +0,0 @@
-import SwapContainer from './SwapContainer';
-import TokenSelector from './TokenSelector';
-import SwapInput from './SwapInput';
-import SwapButton from './SwapButton';
-
-export { SwapContainer, TokenSelector, SwapInput, SwapButton };
